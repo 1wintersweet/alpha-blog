0aa70d5247ca5b0c98d63cb9a7a81e5b3eb1b092
Wintersweet <ge_zhang@hotmail.com>
2016-02-24 15:57:45 +0000

    fix create, add flash and error message, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,10 +8,27 @@ class ArticlesController < ApplicationController
     # render plain
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
+   # @article.save
     #redirect after the saving
-    redirect_to article_show(@article)
+  #  redirect_to article_path(@article)
+    
+    # usual create operation
+    if @article.save
+      # do something
+      # where to display - views are wrapped by application.html.erb -> all message will be there. 
+      flash[:notice] = "article is saved successfully"
+      redirect_to article_path(@article)
+    else
+      # if validation has not passed, re-render the new form
+      render 'new' # reload the same new template
+    end 
   end
+  
+  def show # need to know the id so the object is the article created
+  
+    @article = Article.find(params[:id])
+    
+  end 
   
   private
     def article_params
@@ -19,5 +36,4 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :description)
     end
   
-  
-end
+end 
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@
 
 class Article < ActiveRecord::Base
   # create validation rules
-  validates :title, presence: true, length: {mininum: 3, maximum: 50}
-  validates :description, presence: true, length: {minimum: 3, maximum: 50}
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 3, maximum: 100}
   
 end
