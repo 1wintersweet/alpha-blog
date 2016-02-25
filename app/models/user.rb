2a9bfb71f89b1cb2e3c7d8f1bd7437feff6b6715
Wintersweet <ge_zhang@hotmail.com>
2016-02-25 20:24:12 +0000

    create users table and user model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  # create validation rules
+  #validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  # :description, presence: true, length: {minimum: 3, maximum: 100}
+  
+end
