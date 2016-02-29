c7ebb29c23411ebe19fb94bdfdb8f5553a212c0a
Wintersweet <ge_zhang@hotmail.com>
2016-02-29 23:10:57 +0000

    Add article category associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@
 
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   # create validation rules
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 3, maximum: 100}
